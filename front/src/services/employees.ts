57907730a6c5ce818a0e4116280f3df252dc35d2
Gianni Henriques <gianni.henriques@gmail.com>
2024-09-03 10:28:00 +0200

    feat(services): added remaining employees routes

--- front/src/services/employees.ts
+++ front/src/services/employees.ts
@@ -2,7 +2,7 @@ import { EmployeeDTO, isEmployees, Employee, isEmployee } from '@/types/Employee
 import fetch from 'node-fetch'
 
 export default class EmployeesService {
-    private _route: string = 'https://soul-connection/api/employees/'
+    private _route: string = 'https://localhost:5000/api/employees'
     private _headers = {
         'Content-Type': 'application/json'
     }
@@ -23,7 +23,7 @@ export default class EmployeesService {
     }
 
     public async login(data: { email: string, password: string }): Promise<{ access_token: string }> {
-        const res = await fetch(`${this._route}login`, {
+        const res = await fetch(`${this._route}/login`, {
             method: 'POST',
             body: JSON.stringify(data)
         })
@@ -34,7 +34,7 @@ export default class EmployeesService {
     }
 
     public async getEmployee(data: { id: string }): Promise<Employee> {
-        const res = await fetch(`${this._route}${data.id}`, {
+        const res = await fetch(`${this._route}/${data.id}`, {
             method: 'GET',
             headers: this._headers
         })
@@ -46,5 +46,31 @@ export default class EmployeesService {
             throw Error()
 
         return object
+    }
+
+    public async getEmployeeMe(): Promise<Employee> {
+        const res = await fetch(`${this._route}/me`, {
+            method: 'GET',
+            headers: this._headers
+        })
+        if (!res.ok)
+            throw Error()
+
+        const object = await res.json()
+        if (!isEmployee(object))
+            throw Error()
+
+        return object
+    }
+
+    public async getEmployeeImage(data: { id: string }): Promise<string> {
+        const res = await fetch(`${this._route}${data.id}/image`, {
+            method: 'GET',
+            headers: this._headers
+        })
+        if (!res.ok)
+            throw Error()
+
+        return await res.json()
     }
 }
