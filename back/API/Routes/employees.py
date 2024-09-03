6ea2dc02e11e6cd603472682e748541c36d8a72a
AugustinPif <augustin.piffeteau@epitech.eu>
2024-09-03 17:50:01 +0200

    fix(routes): employees login now return the access_token

--- back/API/Routes/employees.py
+++ back/API/Routes/employees.py
@@ -1,8 +1,45 @@
 from flask import Blueprint, jsonify, request, session
 from dbConnection import db
 from bson.objectid import ObjectId
+import os
+from dotenv import load_dotenv
+import requests
 
 employees_blueprint = Blueprint('employees', __name__)
+
+load_dotenv()
+
+def getAccessToken():
+    api_key = os.getenv("API_KEY")
+    api_email = os.getenv("API_EMAIL")
+    api_password = os.getenv("API_PASSWORD")
+
+    if not api_key or not api_email or not api_password:
+        print("Missing environment variables. Please check your .env file.")
+        return None
+
+    url = "https://soul-connection.fr/api/employees/login"
+    headers = {
+        "X-Group-Authorization": api_key,
+        "Content-Type": "application/json"
+    }
+    payload = {
+        "email": api_email,
+        "password": api_password
+    }
+    response = requests.post(url, headers=headers, json=payload)
+
+    if response.status_code == 200:
+        response_json = response.json()
+        if 'access_token' in response_json:
+            access_token = response_json.get('access_token')
+        else:
+            print(f"Login failed: {response_json.get('error', 'Unknown error')}")
+            access_token = None
+    else:
+        print(f"Failed to login: {response.status_code}\n")
+
+    return access_token
 
 
 @employees_blueprint.route('/api/employees', methods=['GET'])
@@ -13,7 +50,6 @@ def getEmployees():
 
 @employees_blueprint.route('/api/employees/login', methods=['POST'])
 def login():
-    #might change the get of email and password
     data = request.get_json()
     email = data['email']
     password = data['password']
@@ -21,7 +57,8 @@ def login():
     employee = db.Employees.find_one({'email': email})
     if employee and employee['password'] == password:
         session['employee_id'] = str(employee['_id'])
-        return jsonify(employee), 200
+        request.headers.get()
+        return getAccessToken(), 200
     else:
         return jsonify({"detail": "Invalid Email and Password combination."}), 401
 
