ab4fd4e727793a127ac2cc000567698cf5b7b18c
Gianni Henriques <gianni.henriques@gmail.com>
2024-09-03 13:36:31 +0200

    feat(services): add encounters service and encounter typeguards

--- front/src/types/Encounter.ts
+++ front/src/types/Encounter.ts
@@ -13,3 +13,25 @@ export type EncounterDTO = {
     date: string
     rating: number
 }
+
+export function isCustomer(data: unknown): data is Encounter {
+    return !!data &&
+        typeof (data as Encounter).id === 'number' &&
+        typeof (data as Encounter).customer_id === 'string' &&
+        typeof (data as Encounter).date === 'string' &&
+        typeof (data as Encounter).rating === 'string' &&
+        typeof (data as Encounter).comment === 'string' &&
+        typeof (data as Encounter).source === 'string'
+}
+
+export function isCustomerDTO(data: unknown): data is EncounterDTO {
+    return !!data &&
+        typeof (data as EncounterDTO).id === 'number' &&
+        typeof (data as EncounterDTO).customer_id === 'string' &&
+        typeof (data as EncounterDTO).date === 'string' &&
+        typeof (data as EncounterDTO).rating === 'number'
+}
+
+export function isCustomers(datas: unknown): datas is EncounterDTO[] {
+    return Array.isArray(datas) && datas.every(data => isCustomerDTO(data))
+}
