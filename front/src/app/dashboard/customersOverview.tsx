0df797ae2eaeab81aa691c73c460ab32c996ebc9
Sacha Polerowicz <sacha.polerowicz@epitech.eu>
2024-09-06 14:47:27 +0200

    feat(customers overview): link customers stats with api

--- front/src/app/dashboard/customersOverview.tsx
+++ front/src/app/dashboard/customersOverview.tsx
@@ -1,137 +1,113 @@
 'use client';
 
 import React, { useState, useEffect } from "react";
-import { SelectButton } from 'primereact/selectbutton';
 import './dashboard.css';
 import { Chart } from 'primereact/chart';
-import { subDays, format } from 'date-fns';
-import { FaArrowDown, FaArrowUp } from 'react-icons/fa';
 
-interface JustifyOption {
-    date: string;
-    code: string;
-}
+import CustomersService from '../services/customers';
+import { CustomerDTO, Customer } from '../types/Customer'
 
-interface StatisticsGraphProps {
-    timelapsed: string;
-    clientNbr: any;
-}
+const customerService = new CustomersService()
 
 interface StatisticsProps {
     title: string;
     number: number;
-    percentage?: number;
 }
 
 export default function CustomersOverview({ selectedTime }: any) {
     return (
         <div className="card-customers">
             <div className="customers">
-                <CustomersInfo selectedTime={selectedTime} />
+                <CustomersInfo />
             </div>
         </div>
     )
 }
 
-function CustomersInfo({ selectedTime }: any) {
-    const [value, setValue] = useState<JustifyOption>(selectedTime);
-    const justifyOptions: JustifyOption[] = [
-        {date: '7 D', code: '7D'},
-        {date: '1 M', code: '1M'},
-        {date: '3 M', code: '3M'}
-    ];
-
-    const justifyTemplate = (option: JustifyOption) => {
-        return <span className="justifyOption">{option.date}</span>;
-    }
+function CustomersInfo() {
+    const [customers, setCustomers] = useState<CustomerDTO[]>([])
 
     useEffect(() => {
-        setValue(selectedTime);
-    }, [selectedTime]);
+        const getCustomers = async () => {
+            try {
+                setCustomers(await customerService.getCustomers())
+            } catch (error) { }
+        }
+        getCustomers()
+    }, []);
 
     return (
         <div>
             <div className="title-info second-line">
                 Customers Overview
-                <SelectButton className="" value={value} onChange={(e) => setValue(e.value)} itemTemplate={justifyTemplate} optionLabel="value" options={justifyOptions} />
             </div>
-            <div className="subtitle-info"> When customers have joined in the time. </div>
-            <div className="second-line move-bot">
-                <Statistics title="Customers" number={932} percentage={14.27} />
-                <Statistics title="Doing meetings" number={28.49} percentage={-12.37} />
-                <Statistics title="Customers by coach" number={34} />
+            <div className="subtitle-info"> Statistics about all the clients. </div>
+            <div className="second-line move-bot stat-middle">
+                <Statistics title="Total customers" number={customers.length} />
             </div>
-            <StatisticsGraph timelapsed={value ? value.code : ''} clientNbr={[520, 250, 120, 574, 978, 854, 750]}/>
+            <StatisticsGraph customers={customers}/>
         </div>
     )
 }
 
-function StatisticsGraph({timelapsed, clientNbr}: StatisticsGraphProps) {
+function StatisticsGraph({ customers }: { customers: CustomerDTO[] }) {
     const [chartData, setChartData] = useState({});
 
-    const convertTimelapsedToDays = (timelapsed: string) => {
-        switch(timelapsed) {
-            case '7D':
-                return 7;
-            case '1M':
-                return 30;
-            case '3M':
-                return 90;
-            default:
-                return 7;
-        }
-    }
-
-    const generateLabels = (timelapse: string) => {
-        const days = convertTimelapsedToDays(timelapse);
-        const labels = [];
-        for (let i = 0; i < days; i++) {
-            const date = subDays(new Date(), i);
-            labels.unshift(format(date, 'MM/dd/yyyy'));
-        }
-        return labels;
-    }
-
     useEffect(() => {
-        const data = {
-            labels: generateLabels(timelapsed),
-            datasets: [
-                {
-                    label: 'Number of clients',
-                    data: clientNbr,
-                    fill: true,
-                    borderColor: '#2622E8',
-                    tension: 0.4,
-                    backgroundColor: 'rgba(71,68,241,0.2)'
-                }
-            ]
+        const fetchCustomers = async () => {
+            const genders = { Male: 0, Female: 0, Other: 0 };
+
+            const promises = customers.map(customer =>
+                customerService.getCustomer({ id: customer.id }).catch(error => {
+                    console.error(error);
+                    return null;
+                })
+            );
+
+            const results = await Promise.all(promises);
+
+            for (let result of results)
+                if (result && result.gender in genders)
+                    genders[result.gender as 'Male' | 'Female' | 'Other']++;
+
+            const data = {
+                labels: Object.keys(genders),
+                datasets: [
+                    {
+                        label: 'Genders',
+                        data: Object.values(genders),
+                        backgroundColor: [
+                            'rgba(255, 159, 64, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(54, 162, 235, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgb(255, 159, 64)',
+                            'rgb(75, 192, 192)',
+                            'rgb(54, 162, 235)'
+                        ],
+                        borderWidth: 1
+                    }
+                ]
+            };
+            setChartData(data);
         };
 
-        setChartData(data);
-    }, [timelapsed]);
+        fetchCustomers();
+    }, [customers]);
 
     return (
         <div className="card">
-            <Chart type="line" data={chartData} options={{ scales: { y: { min: 0, max: 1200 } }, legend: {display: false} }} />
+            <Chart type="bar" data={chartData} />
         </div>
     )
 }
 
-function Statistics({ title, number, percentage = 0 }: StatisticsProps) {
-    const percentageClass = percentage < 0 ? 'negative' : 'positive';
-    const absolutePercentage = Math.abs(percentage);
-
+function Statistics({ title, number }: StatisticsProps) {
     return (
         <div>
             <div className="subtitle-info">{title}</div>
             <div className="number">{number}</div>
-            {percentage !== 0 && (
-                <div className={`number ${percentageClass}`}>
-                    {percentage < 0 ? <FaArrowDown /> : <FaArrowUp />}
-                    {' '}
-                    {`${absolutePercentage}%`}
-                </div>
-            )}
         </div>
     )
 }
