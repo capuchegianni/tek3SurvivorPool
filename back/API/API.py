43f652bde59011898df23080dcaa5dae7a6863ec
Sacha Polerowicz <sacha.polerowicz@epitech.eu>
2024-09-06 12:29:37 +0200

    fix(fetch): fix issues with fetch

--- back/API/API.py
+++ back/API/API.py
@@ -14,6 +14,7 @@ def createAPI():
     app.config['JWT_COOKIE_SECURE'] = False
     app.config['JWT_SESSION_COOKIE'] = True
     app.config['JWT_COOKIE_CSRF_PROTECT'] = False
+    app.config['SAMESITE'] = 'Lax'
     jwt.init_app(app)
     CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000", "supports_credentials": True}})
 
